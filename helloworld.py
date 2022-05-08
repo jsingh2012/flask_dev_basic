9a137056e420b75d19b6e9384548a5b18c2ce6b9
jsingh2012 <jatinderpalsinghtu@gmail.com>
2022-05-08 17:31:35 +0530

    Basic template code. Code clean up required

--- helloworld.py
+++ helloworld.py
@@ -1,22 +1,30 @@
 from flask import Flask, render_template
 from flask import request
 from flask import redirect
+from flask_bootstrap import Bootstrap
+import datetime
 from flask import abort
+import os
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
-    return render_template('index.html')
+    return render_template('index.html', utc_dt=datetime.datetime.utcnow())
 
-@app.route('/user/<name>')
-def user(name):
-    return redirect('http://youtube.com')
+@app.route('/user/')
+def user():
+    return render_template('user.html')
 
 @app.route('/userid/<id>')
 def get_user(id):
-    return render_template('user.html', name=id)
+    return render_template('user.html', user=id)
 
 def load_user(id):
     return None
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
